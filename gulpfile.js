1cbc709b892f464207f517a2fcbb429e782ea4e2
Jake Archibald <jaffathecake@gmail.com>
2017-05-21 09:16:13 -0700

    Rough build

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const less = require('gulp-less');
+const babel = require('gulp-babel');
+
+const paths = {
+  scripts: {
+    src: 'src/**/*.js',
+    dest: 'dist/'
+  }
+};
+
+function clean() {
+  return del(['dist']);
+}
+
+function scripts() {
+  return gulp.src(paths.scripts.src, {
+      sourcemaps: true,
+      since: gulp.lastRun(scripts)
+    })
+    .pipe(babel({
+      "plugins": [
+        ["transform-react-jsx"]
+      ]
+    }))
+    .pipe(gulp.dest(paths.scripts.dest));
+}
+
+function watch() {
+  gulp.watch(paths.scripts.src, scripts);
+}
+
+exports.build = gulp.series(clean, scripts);
+exports.dev = gulp.series(exports.build, watch);
---
+++ src/presentation/index.js
@@ -0,0 +1,8 @@
+/** @jsx dom */
+import {dom} from '../utils/dom.js';
+
+const element = (
+  <hello-world foo="bar">
+    Hello
+  </hello-world>
+);
