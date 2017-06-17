b6ea801543a571530a937359ffc60761f74dc477
Jake Archibald <jaffathecake@gmail.com>
2017-06-17 12:00:12 -0400

    Fade out & attributes

--- src/utils/anims/index.js
+++ src/utils/anims/index.js
@@ -5,16 +5,15 @@ export async function fadeIn(element, {
   easing = easeInOutQuad
 }={}) {
   const slide = element.closest('preso-slide');
-
   if (!slide) throw Error("Fading element must be within preso-slide");
   
   element.style.opacity = 0.0001;
-  
+
   await slide.synchronize();
 
   element.style.opacity = '';
   
-  element.animate([
+  return element.animate([
     {opacity: 0.0001},
     {opacity: 1}
   ], {
@@ -22,11 +21,43 @@ export async function fadeIn(element, {
   }).finished;
 }
 
+export async function fadeOut(element, {
+  duration = 500,
+  easing = easeInOutQuad
+} = {}) {
+  const slide = element.closest('preso-slide');
+  if (!slide) throw Error("Fading element must be within preso-slide");
+
+  await slide.synchronize();
+
+  return element.animate([
+    { opacity: 1 },
+    { opacity: 0 }
+  ], {
+    duration, easing,
+    fill: 'forwards'
+  }).finished;
+}
+
 export function animWatcher(element) {
   new MutationObserver(records => {
     for (const {addedNodes} of records) {
       for (const node of addedNodes) {
+        // fade-in etc
         if (node.nodeType == 1 && node.hasAttribute('fade-in')) {
+          node.removeAttribute('fade-in');
+          const opts = {};
+
+          if (node.hasAttribute('fade-duration')) {
+            opts.duration = Number(node.getAttribute('fade-duration'));
+            node.removeAttribute('fade-duration')
+          }
+
+          if (node.hasAttribute('fade-easing')) {
+            opts.easing = node.getAttribute('fade-easing');
+            node.removeAttribute('fade-easing')
+          }
+
           fadeIn(node);
         }
       }
